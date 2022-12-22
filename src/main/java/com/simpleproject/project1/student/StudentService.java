029f458ca9b95663cee44a29ecdd216ce300698a
sowmya <k.l.sowmya219@gmail.com>
2022-12-22 20:59:21 +0100

    first commit

---
+++ src/main/java/com/simpleproject/project1/student/StudentService.java
@@ -0,0 +1,21 @@
+package com.simpleproject.project1.student;
+
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+@Service
+public class StudentService {
+    public List<Student> getStudents() {
+        return  List.of(
+                new Student(
+                        1L,
+                        "Mariam",
+                        "Mariam@gmail.com",
+                        LocalDate.of(2000, Month.JANUARY,21),
+                        21
+                )
+        );
+    }
+}
